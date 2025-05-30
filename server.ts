e3ef92e6131670423bfdf7a0fa2873ef006701a4
Zdravko Iliev <zdravko.iliev@vereign.com>
2025-05-30 12:03:38 +0300

    fix: 1.0.6

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ function ss() {
   const PORT = 3000;
 
   app.get("/api/hello", (_, res) => {
-    res.send("Hello World from Express! hello 1.0.5");
+    res.send("Hello World from Express! hello 1.0.6");
   });
 
   app.listen(PORT, () => {
